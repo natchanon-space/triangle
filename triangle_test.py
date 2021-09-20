ec1811b14f586b82a085e39ad4a04afd6f9d4221
natchanon-space <natchanon.space@gmail.com>
2021-09-20 10:56:01 +0700

    exercise 1

--- triangle_test.py
+++ triangle_test.py
@@ -10,12 +10,14 @@ class TriangleTest(unittest.TestCase):
         self.assertTrue( is_triangle(8, 10, 12) )
         self.assertTrue( is_triangle(100, 101, 200) )
         self.assertTrue( is_triangle(0.9, 1.0, 1.1) )
+        self.assertTrue( is_triangle(1, 1, 4) ) # for code coverage
 
     def test_not_triangle(self):
         self.assertFalse( is_triangle(21, 10, 10) )
         self.assertFalse( is_triangle(2, 1, 1) )   # borderline case
         self.assertFalse( is_triangle(6, 10, 4) )  # borderline case
         self.assertFalse( is_triangle(6, 20, 4) )
+        self.assertFalse( is_triangle(1, 1, 4) ) # for code coverage
 
     def test_invalid_argument_raises_exception(self):
         """any non-positive argument should raise ValueError"""
